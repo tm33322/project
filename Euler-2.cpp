868f5747e3185649853e61c6c001f6354ce84a0a
tm33322 <34222019+tm33322@users.noreply.github.com>
2017-12-09 12:33:03 -0600

    IntoducePlots

---
+++ Euler-2.cpp
@@ -0,0 +1,63 @@
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+using namespace std;
+ 
+typedef double F(double,double);	//Definition of the F type as an array of 2 double
+
+double mode, y0, a, b, h, x[5], r;	//If mode=1, we are in normal mode
+					//if mode=2, we are in debug mode
+int i;
+
+void inputdata()	//Treatment of input data
+{
+
+	i=0;
+	ifstream infile("input.dat");
+
+	while (infile >> r)
+	{
+		x[i]=r;
+		i++;
+	}
+
+	mode=x[0];
+	y0=x[1];
+	a=x[2];
+	b=x[3];
+	h=x[4];
+
+}
+ 
+/*
+Approximates y(t) in y'(t)=f(t,y) with y(a)=y0 and
+t=a..b and the step size h.
+*/
+void euler(F f, double y0, double a, double b, double h)
+{
+	ofstream outputfile;
+	outputfile.open("output.dat");
+
+    double y = y0;
+    for (double t = a; t < b; t += h)
+    {
+        std::cout << std::fixed << std::setprecision(3) << t << " " << y << "\n";
+	outputfile << t << " " << y << endl;
+        y += h * f(t, y);
+    }
+    std::cout << "done\n";
+	outputfile.close();
+}
+ 
+// Example: dy/dt=y
+double newtonCoolingLaw(double t, double y)	//The function receives 2 arguments of type double
+						//This is a general form, we only use 1 argument here
+{
+    return y;
+}
+ 
+int main()
+{
+	inputdata();
+	euler(newtonCoolingLaw, y0, a, b, h);
+}
